90bcedbad1ac5805a7c7406a260851ee0fc88c4b
kamatozXIII <kamatozXIII@gmail.com>
2022-05-16 20:53:33 +0300

    add changes dashboard add logo

--- src/views/dashboard/Dashboard.vue
+++ src/views/dashboard/Dashboard.vue
@@ -1,26 +1,62 @@
 <template>
   <div>
-    <v-row class="justify-center" >
+    <v-row class='justify-center'>
       <v-col
         d-flex
-        cols="6"
+        cols='6'
       >
-        <v-card class="px-8 pt-12 pb-12 bg-gradient-primary">
+        <v-card class='px-8 pt-12 pb-12 patyson-card'>
           <h2 class='text-center mb-5'>Patyson Beach Club</h2>
-          <h4 class="text-center mb-2">Введіть номер картки</h4>
+          <h4 class='text-center mb-2'>Введіть номер картки</h4>
           <v-otp-input
-            length="10"
+            length='10'
             outlined
           ></v-otp-input>
         </v-card>
       </v-col>
     </v-row>
-    <v-row class="justify-center">
-      <v-col cols="6">
+    <v-row class='justify-center'>
+      <v-col cols='6'>
+
         <v-card
-          class="py-6"
+          class='py-6 patyson-card-pay'
         >
-          <div class="py-5 text-center" v-html="html" />
+          <h3 class='price'>Ціна за один день: 200грн.</h3>
+          <v-container fluid>
+            <v-row align='center'>
+              <v-col cols='6'>
+                <h3>
+                  Кількість днів
+                </h3>
+              </v-col>
+
+              <v-col cols='6'>
+
+                <v-select
+                  v-model='select'
+                  :items='number'
+                  item-text='state'
+                  item-value='abbr'
+                  label='Select'
+                  persistent-hint
+                  return-object
+                  single-line
+                ></v-select>
+              </v-col>
+            </v-row>
+          </v-container>
+          <h3 class='text-start pa-3'>Загальна вартість: <v-textarea
+            auto-grow
+            outlined
+          ></v-textarea></h3>
+          <v-row class='d-flex'>
+            <v-col
+              cols='4' class='text-end align-self-center'>
+              Оплатити:
+            </v-col>
+            <v-col cols='8' class='py-5 text-center' v-html='html' />
+          </v-row>
+
         </v-card>
       </v-col>
     </v-row>
@@ -33,6 +69,8 @@ import LiqPay from '@/utils/liqpay'
 export default {
   data() {
     return {
+      select: { state: 'Florida' },
+      number: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
       liqpay: null,
       html: null,
     }
@@ -55,5 +93,11 @@ export default {
 }
 </script>
 <style>
+.patyson-card {
+  background: linear-gradient(240deg, #05ddf1, #f67501 94%)
+}
 
+.patyson-card-pay {
+  background: linear-gradient(40deg, #05ddf1, #f67501 94%)
+}
 </style>
